0d2f535049979c0ac77ec87584040bd230370827
Josh Bailey <josh@vandervecken.com>
2025-10-26 23:55:32 +1300

    mido + proxy

--- preframr/sidwav.py
+++ preframr/sidwav.py
@@ -1,9 +1,12 @@
+from collections import defaultdict
 from datetime import timedelta
+import time
 from tqdm import tqdm
 from scipy.io import wavfile
 from pyresidfp import SoundInterfaceDevice
 from pyresidfp.sound_interface_device import ChipModel
 import pandas as pd
+import mido
 import numpy as np
 from preframr.stfconstants import (
     CTRL_REG,
@@ -11,16 +14,120 @@ from preframr.stfconstants import (
     FRAME_REG,
     RESET_REG,
     VOICE_REG,
-    VOICES,
     VOICE_REG_SIZE,
     MODE_VOL_REG,
     MAX_REG,
 )
 
+ID_REG = {
+    0: 0,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 5,
+    5: 6,
+    6: 7,
+    7: 8,
+    8: 9,
+    9: 10,
+    10: 12,
+    11: 13,
+    12: 14,
+    13: 15,
+    14: 16,
+    15: 17,
+    16: 19,
+    17: 20,
+    18: 21,
+    19: 22,
+    20: 23,
+    21: 24,
+    22: 4,
+    23: 11,
+    24: 18,
+}
+
+ELEKTRON_MANID = 0x2D
+ASID_START = 0x4C
+ASID_STOP = 0x4D
+ASID_UPDATE = 0x4E
+
+
+class AsidProxy:
+    def __init__(self, sid, port, in_port=None, update_cmd=ASID_UPDATE):
+        self.sid = sid
+        self.port = port
+        self.in_port = in_port
+        self.update_cmd = update_cmd
+        if self.in_port:
+            while self.in_port.poll():
+                self.in_port.receive()
+        self._resetreg()
+
+    def _resetreg(self):
+        self.regs = defaultdict(int)
+        self.pending_regs = defaultdict(int)
+
+    @property
+    def clock_frequency(self):
+        return self.sid.clock_frequency
+
+    @property
+    def sampling_frequency(self):
+        return self.sid.sampling_frequency
+
+    def write_register(self, reg, val):
+        self.sid.write_register(reg, val)
+        self.pending_regs[reg] = val
+
+    def clock(self, seconds):
+        self.update(seconds)
+        return self.sid.clock(seconds)
+
+    def _sysex(self, data):
+        msg = mido.Message("sysex", data=[ELEKTRON_MANID] + data)
+        self.port.send(msg)
+        if self.in_port:
+            self.in_port.receive()
+
+    def start(self):
+        self._sysex([ASID_START])
+        self._resetreg()
+
+    def stop(self):
+        self._sysex([ASID_STOP])
+        self._resetreg()
+
+    def update(self, seconds):
+        masks = [0, 0, 0, 0]
+        msbs = [0, 0, 0, 0]
+        vals = []
+
+        for reg_id, reg in sorted(ID_REG.items()):
+            new_val = self.pending_regs.get(reg, None)
+            if new_val is None:
+                continue
+            if new_val == self.regs[reg]:
+                continue
+            self.regs[reg] = new_val
+            meta_byte = int(reg_id / 7)
+            meta_bit = reg_id % 7
+            masks[meta_byte] |= 2**meta_bit
+            if new_val & 0x80:
+                msbs[meta_byte] |= 2**meta_bit
+            vals.append(new_val & 0x7F)
+        if vals:
+            self._sysex([self.update_cmd] + masks + msbs + vals)
+        time.sleep(seconds)
+
+
+def default_sid():
+    return SoundInterfaceDevice(model=ChipModel.MOS8580)
+
 
 def sidq(sid=None):
     if sid is None:
-        sid = SoundInterfaceDevice()
+        sid = default_sid()
     return sid.clock_frequency / 1e6 / 1e6
 
 
@@ -31,9 +138,10 @@ def write_reg(sid, reg, val, reg_widths):
         val >>= 8
 
 
-def write_samples(orig_df, name, reg_widths, reg_start=None, irq=None):
+def write_samples(orig_df, name, reg_widths, reg_start=None, irq=None, sid=None):
     df = orig_df.copy()
-    sid = SoundInterfaceDevice(model=ChipModel.MOS8580)
+    if sid is None:
+        sid = default_sid()
     if reg_start is None:
         reg_start = {MODE_VOL_REG: 15}
         for v in range(3):
