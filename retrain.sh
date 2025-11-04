9ac67edc459195c0e6e9e8fc879ff516268e9c88
Josh Bailey <josh@vandervecken.com>
2025-11-05 08:46:59 +1300

    jet.

--- retrain.sh
+++ retrain.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 docker rm -f preframr-train
 sudo rm -rf /scratch/preframr/tb_logs/*
-./build.sh && ./train.sh $* && docker logs -f preframr-train
+#./build.sh &&
+./train.sh $* --seq-len 8192 --max-seq-len 16384 --batch-size 16 --reglogs '/scratch/preframr/training-dumps/MUSICIANS/G/Goto80/*zst' --shuffle 0.1 --layers 8 --heads 8 --kv-heads 8 --embed 256 && docker logs -f preframr-train
