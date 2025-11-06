1c6f2c1baf66acc95dc714c3a6ff45805a165072
Josh Bailey <josh@vandervecken.com>
2025-11-06 23:59:10 +0000

    args.

--- preframr/args.py
+++ preframr/args.py
@@ -29,7 +29,7 @@ def add_args(parser):
     parser.add_argument("--max-seq-len", type=int, default=32768)
     parser.add_argument("--prompt-seq-len", type=int, default=2048)
     parser.add_argument("--max-epochs", type=int, default=1000)
-    parser.add_argument("--batch-size", type=int, default=8)
+    parser.add_argument("--batch-size", type=int, default=16)
     parser.add_argument("--tkvocab", type=int, default=0)
     parser.add_argument("--wav", type=str, default="/scratch/preframr/preframr.wav")
     parser.add_argument("--csv", type=str, default="/scratch/preframr/preframr.csv.zst")
@@ -55,7 +55,7 @@ def add_args(parser):
     parser.add_argument("--layers", type=int, default=12)
     parser.add_argument("--heads", type=int, default=12)
     parser.add_argument("--kv-heads", type=int, default=12)
-    parser.add_argument("--embed", type=int, default=768)
+    parser.add_argument("--embed", type=int, default=384)
     parser.add_argument("--intermediate", type=int, default=None)
     parser.add_argument("--norm-eps", type=float, default=1e-5)
     parser.add_argument("--rope-base", type=float, default=5e5)
@@ -80,8 +80,8 @@ def add_args(parser):
         default="bfloat16",
         choices=list(MODEL_PRECISION.keys()),
     )
-    parser.add_argument("--weight-decay", type=float, default=1e-2)
-    parser.add_argument("--accumulate-grad-batches", type=int, default=8)
+    parser.add_argument("--weight-decay", type=float, default=1e-4)
+    parser.add_argument("--accumulate-grad-batches", type=int, default=4)
     parser.add_argument("--log-every-n-steps", type=int, default=2)
     parser.add_argument("--max-perm", type=int, default=99)
     parser.add_argument("--ckpt-hours", type=int, default=12)
