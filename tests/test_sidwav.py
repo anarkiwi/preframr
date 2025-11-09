e2543d1a507a07a6de3511c09cce141dffbc8c8d
Josh Bailey <josh@vandervecken.com>
2025-11-09 15:51:57 +1300

    ring/sync effect

--- tests/test_sidwav.py
+++ tests/test_sidwav.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 import os
 import random
 import tempfile
@@ -6,7 +7,8 @@ import numpy as np
 import pandas as pd
 import scipy
 
-from preframr.sidwav import write_samples
+from pyresidfp import SoundInterfaceDevice
+from preframr.sidwav import write_samples, default_sid
 
 
 class TestSidwav(unittest.TestCase):
@@ -22,3 +24,60 @@ class TestSidwav(unittest.TestCase):
             self.assertEqual(rate, 48000)
             data = np.round(data, 2)
             self.assertTrue(abs(data.sum()))
+
+    def test_ring_effects(self):
+        def _test_samples(ctrl_val):
+            sid = SoundInterfaceDevice(sampling_frequency=8e3)
+            sec = timedelta(seconds=1)
+            sid.write_register(24, 15)
+            sid.write_register(6, 240)
+            sid.write_register(0, 8)
+            sid.write_register(1, 8)
+            sid.write_register(20, 240)
+            sid.write_register(14, 4)
+            sid.write_register(15, 4)
+            sid.clock(sec)
+            sid.write_register(4, ctrl_val)
+            samples = sid.clock(sec)
+            return np.array(samples)
+
+        ring = 2**2
+        gate = 2**0
+
+        # ring affects tri
+        waveform = 16
+        a = _test_samples(waveform + gate)
+        b = _test_samples(waveform + gate + ring)
+        assert not np.allclose(a, b, rtol=2, atol=2)
+
+        # ring does not affect other
+        for waveform in (32, 64, 128):
+            a = _test_samples(waveform + gate)
+            b = _test_samples(waveform + gate + ring)
+            assert np.allclose(a, b, rtol=2, atol=2)
+
+    def test_sync_effects(self):
+        def _test_samples(ctrl_val1, ctrl_val2):
+            sid = SoundInterfaceDevice(sampling_frequency=8e3)
+            sec = timedelta(seconds=1)
+            sid.write_register(24, 15)
+            sid.write_register(6, 240)
+            sid.write_register(0, 8)
+            sid.write_register(1, 8)
+            sid.write_register(20, 240)
+            sid.write_register(14, 4)
+            sid.write_register(15, 4)
+            sid.write_register(4, ctrl_val1)
+            sid.clock(sec)
+            sid.write_register(4, ctrl_val2)
+            samples = sid.clock(sec)
+            return np.array(samples)
+
+        sync = 2**1
+        gate = 2**0
+
+        # sync with no waveform has no effect.
+        for waveform in (16, 32, 64, 128):
+            a = _test_samples(waveform + gate, 0)
+            b = _test_samples(waveform + gate, sync)
+            assert np.allclose(a, b, rtol=2, atol=2)
