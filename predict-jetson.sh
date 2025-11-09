3eb65b068d3bc1c15098307d66f77b63b9d7a37b
Josh Bailey <josh@vandervecken.com>
2025-11-09 01:24:14 +0000

    jet.

--- predict-jetson.sh
+++ predict-jetson.sh
@@ -2,9 +2,5 @@
 set -o noglob
 
 IMG=anarkiwi/preframr-jetson
-FLAGS=""
-NVGPUS=$(nvidia-smi -L 2>/dev/null)
-if [[ $? -eq 0 && ! -z "$NVGPUS" ]] ; then
-    FLAGS=--runtime=nvidia
-fi
+FLAGS=--runtime=nvidia
 exec docker run $FLAGS -eTORCHINDUCTOR_FX_GRAPH_CACHE=1 -eTORCHINDUCTOR_CACHE_DIR=/scratch/preframr/inductorcache --rm --name preframr-predict -v /scratch:/scratch --device /dev/snd -ti $IMG /preframr/predict.py $*
