93e78cfb49fed05c3b9ef650627a2025524f5082
Josh Bailey <josh@vandervecken.com>
2025-11-14 02:40:39 +0000

    chown.

--- retrain.sh
+++ retrain.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 docker rm -f preframr-train
+ID=$(id -u)
 while true ; do
-  sudo rm -rf /scratch/preframr/tb_logs/*
+  sudo chown -R ${ID} /scratch/preframr/tb_logs || true
+  rm -rf /scratch/preframr/tb_logs/*
   if [ $? -eq 0 ]; then break ; fi
   sleep 1
 done
