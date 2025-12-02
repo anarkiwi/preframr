1bfd48c038c1444f4bb95fd3fa8b6111974505d3
Josh Bailey <josh@vandervecken.com>
2025-12-02 01:26:17 +0000

    no token.

--- preframr/args.py
+++ preframr/args.py
@@ -22,8 +22,8 @@ def add_args(parser):
     parser.add_argument("--max-seq-len", type=int, default=32768)
     parser.add_argument("--prompt-seq-len", type=int, default=2048)
     parser.add_argument("--max-epochs", type=int, default=1000)
-    parser.add_argument("--batch-size", type=int, default=16)
-    parser.add_argument("--tkvocab", type=int, default=8192)
+    parser.add_argument("--batch-size", type=int, default=32)
+    parser.add_argument("--tkvocab", type=int, default=0)
     parser.add_argument("--wav", type=str, default="/scratch/preframr/preframr.wav")
     parser.add_argument("--csv", type=str, default=None)
     parser.add_argument(
@@ -72,7 +72,7 @@ def add_args(parser):
         choices=list(MODEL_PRECISION.keys()),
     )
     parser.add_argument("--weight-decay", type=float, default=1e-2)
-    parser.add_argument("--accumulate-grad-batches", type=int, default=4)
+    parser.add_argument("--accumulate-grad-batches", type=int, default=2)
     parser.add_argument("--log-every-n-steps", type=int, default=2)
     parser.add_argument("--max-perm", type=int, default=99)
     parser.add_argument("--ckpt-hours", type=int, default=12)
