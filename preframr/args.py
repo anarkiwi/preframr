3cfd45c2a2fb6f410f0119c85f36115fbc02fb0d
Josh Bailey <josh@vandervecken.com>
2025-12-07 22:41:38 +0000

    batch.

--- preframr/args.py
+++ preframr/args.py
@@ -36,7 +36,7 @@ def add_args(parser):
         type=str,
         default="/scratch/preframr/dataset-map.csv.zst",
     )
-    parser.add_argument("--shuffle", type=float, default=0.005)
+    parser.add_argument("--shuffle", type=float, default=0.0025)
     parser.add_argument("--max-files", type=int, default=8192)
     parser.add_argument("--min-dump-size", type=int, default=int(1e5))
     parser.add_argument("--min-irq", type=int, default=int(1.5e4))
