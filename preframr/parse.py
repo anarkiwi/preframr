90e5e48d00d636c2e082d86ccb623cf23d6ec962
Josh Bailey <josh@vandervecken.com>
2025-12-29 14:33:27 +1300

    reset.

--- preframr/parse.py
+++ preframr/parse.py
@@ -5,6 +5,7 @@ import concurrent.futures
 import multiprocessing
 from torchtune.utils import get_logger
 from tqdm import tqdm
+from tqdm.contrib.logging import logging_redirect_tqdm
 from preframr.args import add_args
 from preframr.regdataset import glob_dumps
 from preframr.reglogparser import RegLogParser
@@ -22,18 +23,19 @@ def write_df(args, name):
 def main():
     parser = add_args(argparse.ArgumentParser())
     args = parser.parse_args()
-    with concurrent.futures.ProcessPoolExecutor(
-        max_workers=multiprocessing.cpu_count()
-    ) as executor:
-        futures = []
-        for name in glob_dumps(
-            args.reglogs, args.max_files, args.min_dump_size, require_pq=False
-        ):
-            futures.append(executor.submit(write_df, args, name))
-        for future in tqdm(
-            concurrent.futures.as_completed(futures), total=len(futures), maxinterval=1
-        ):
-            assert not future.exception(), future.exception()
+    with logging_redirect_tqdm():
+        with concurrent.futures.ProcessPoolExecutor(
+            max_workers=multiprocessing.cpu_count()
+        ) as executor:
+            futures = []
+            for name in glob_dumps(
+                args.reglogs, args.max_files, args.min_dump_size, require_pq=False
+            ):
+                futures.append(executor.submit(write_df, args, name))
+            with tqdm(total=len(futures)) as t:
+                for future in concurrent.futures.as_completed(futures):
+                    assert not future.exception(), future.exception()
+                    t.update(1)
 
 
 if __name__ == "__main__":
