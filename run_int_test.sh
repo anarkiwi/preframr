c38996531b2202b9f1acb989752dc147cc6c668e
Josh Bailey <josh@vandervecken.com>
2026-02-13 01:38:28 +0000

    fix test.

--- run_int_test.sh
+++ run_int_test.sh
@@ -23,7 +23,7 @@ mkdir -p ${ROOT}
 wget -O${ROOT}/test.sid ${TEST_SID}
 docker run --rm -v ${ROOT}:/scratch/preframr -t anarkiwi/headlessvice /usr/local/bin/vsiddump.py --dumpdir=/scratch/preframr --sid /scratch/preframr/test.sid -tune 1 -limitcycles 60000000
 
-ls -l ${ROOT}/test.None.dump.zst
+ls -l ${ROOT}/test.None.dump.parquet
 
 # GPU if any
 FLAGS=""
@@ -34,6 +34,6 @@ fi
 
 ./build.sh
 # train to the stop loss.
-docker run ${FLAGS} --rm --name preframr-train-test -v ${ROOT}:/scratch/preframr -ti ${IMG} /preframr/train.py --model=llama3_2 --shuffle 1 --layers 4 --heads 4 --kv-heads 4 --embed 128 --batch-size 64 --seq-len 512 --min-dump-size 1 --no-require-pq --accumulate-grad-batches 1 --tkvocab 0 --stop-loss ${STOP_LOSS} --reglogs /scratch/preframr/test.None.dump.zst --dataset-csv /scratch/preframr/dataset.csv.zst --token-csv /scratch/preframr/tokens.csv --df-map-csv /scratch/preframr/df-map.csv
+docker run ${FLAGS} --rm --name preframr-train-test -v ${ROOT}:/scratch/preframr -ti ${IMG} /preframr/train.py --model=llama3_2 --shuffle 1 --layers 4 --heads 4 --kv-heads 4 --embed 128 --batch-size 64 --seq-len 512 --min-dump-size 1 --no-require-pq --accumulate-grad-batches 1 --tkvocab 0 --stop-loss ${STOP_LOSS} --reglogs /scratch/preframr/test.None.dump.parquet --dataset-csv /scratch/preframr/dataset.csv.zst --token-csv /scratch/preframr/tokens.csv --df-map-csv /scratch/preframr/df-map.csv
 # predict with min accuracy.
-docker run ${FLAGS} --rm --name preframr-predict-test -v ${ROOT}:/scratch/preframr -ti ${IMG} /preframr/predict.py --prompt-seq-len 256 --max-seq-len 512 --start-n 0 --min-acc ${MIN_ACC} --no-require-pq --reglog /scratch/preframr/test.None.dump.zst
+docker run ${FLAGS} --rm --name preframr-predict-test -v ${ROOT}:/scratch/preframr -ti ${IMG} /preframr/predict.py --prompt-seq-len 256 --max-seq-len 512 --start-n 0 --min-acc ${MIN_ACC} --no-require-pq --reglog /scratch/preframr/test.None.dump.parquet
