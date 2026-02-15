c4e48b401cef463443b481dc82be955f52b0f4da
Josh Bailey <josh@vandervecken.com>
2026-02-15 03:41:45 +0000

    faster.

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 set -e
 # PIP_OPTS="--index-url http://192.168.5.1:5001/index/ --trusted-host 192.168.5.1"
 
-docker build --build-arg PIP_OPTS="$PIP_OPTS" -f Dockerfile.tensorboard . -t anarkiwi/tensorboard
+docker build --build-arg PIP_OPTS="$PIP_OPTS" -f Dockerfile.tensorboard . -t anarkiwi/tensorboard &
+B0=$!
 
 NVGPUS=$(nvidia-smi --query-gpu name --format=csv,noheader 2>/dev/null|head -1|cut -f 1 -d" ")
 if [[ ! -z "${NVGPUS}" ]] ; then
@@ -12,5 +13,11 @@ if [[ ! -z "${NVGPUS}" ]] ; then
     fi
 fi
 
-docker build --build-arg PIP_OPTS="$PIP_OPTS" -f Dockerfile . -t anarkiwi/preframr
-docker build --build-arg PIP_OPTS="$PIP_OPTS" --build-arg="BASE=anarkiwi/pytorch-xpu:v2.10.0" -f Dockerfile . -t anarkiwi/preframr-xpu
+docker build --build-arg PIP_OPTS="$PIP_OPTS" -f Dockerfile . -t anarkiwi/preframr &
+B1=$!
+docker build --build-arg PIP_OPTS="$PIP_OPTS" --build-arg="BASE=anarkiwi/pytorch-xpu:v2.10.0" -f Dockerfile . -t anarkiwi/preframr-xpu &
+B2=$!
+
+wait $B0
+wait $B1
+wait $B2
